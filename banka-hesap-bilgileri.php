26e10cf6779c703181dfb502f4822af801bf85f8
mcdcoin <36205327+mcdcoin@users.noreply.github.com>
2018-11-26 20:44:06 +0300

    Initial commit

---
+++ banka-hesap-bilgileri.php
@@ -0,0 +1,44 @@
+<?php
+	/* ****** ***********************************  *
+	** ****** POSİstanbul Teknoloji				   *
+	** ****** WHMCS 7 Conflex Premium Style 	   *
+	** ****** v1.0.0 - Tüm hakları saklıdır.       *
+	** ****** www.posistanbul.com 				   *
+	** ****** R10.NET @HasanHuseyin				   *
+	** ****** *********************************** */
+
+	use WHMCS\ClientArea;
+	use WHMCS\Database\Capsule;
+
+	define("CLIENTAREA", true);
+
+	require_once 'init.php';
+
+	$ClientArea = new ClientArea();
+	$ClientArea -> setPageTitle("Banka Hesap Bilgileri");
+	$ClientArea -> addToBreadCrumb("index.php", Lang::trans("globalsystemname"));
+	$ClientArea -> addToBreadCrumb("banka-hesap-bilgileri.php", "");
+
+	$ClientArea -> initPage();
+
+	// Eğer sadece müşteriler görecekse sayfayı
+	//$ClientArea -> requireLogin();
+
+	// Giriş Durumu
+	if ( $ClientArea -> isLoggedIn() ) {
+		// Giriş yapıldı
+		$kayitAdi = Capsule::table("tblclients") -> where("id", "=", $ClientArea -> getUserID()) -> pluck("firstname");
+		$ClientArea -> assign("clientname", $kayitAdi);
+	}else{
+		// Eğer giriş yapılmamış ise
+		// kayıt adına ne yazılmasını isterseniz
+		// standart olarak "Ziyaretçi" olarak geçer.
+		$ClientArea -> assign("clientname", "Ziyaretçi");
+	}
+
+	// TPL dosyasını yükle
+	// linux-hosting.tpl --> linux-hosting gibi düzenlenir
+	$ClientArea -> setTemplate("banka-hesap-bilgileri");
+
+	$ClientArea -> output();
+?>
---
+++ bayi-hosting.php
@@ -0,0 +1,44 @@
+<?php
+	/* ****** ***********************************  *
+	** ****** POSİstanbul Teknoloji				   *
+	** ****** WHMCS 7 Conflex Premium Style 	   *
+	** ****** v1.0.0 - Tüm hakları saklıdır.       *
+	** ****** www.posistanbul.com 				   *
+	** ****** R10.NET @HasanHuseyin				   *
+	** ****** *********************************** */
+
+	use WHMCS\ClientArea;
+	use WHMCS\Database\Capsule;
+
+	define("CLIENTAREA", true);
+
+	require_once 'init.php';
+
+	$ClientArea = new ClientArea();
+	$ClientArea -> setPageTitle("Multi Platform Bayi Hosting");
+	$ClientArea -> addToBreadCrumb("index.php", Lang::trans("globalsystemname"));
+	$ClientArea -> addToBreadCrumb("bayi-hosting.php", "İster Windows, İster Linux ihtiyacına uygun olanı seç.");
+
+	$ClientArea -> initPage();
+
+	// Eğer sadece müşteriler görecekse sayfayı
+	//$ClientArea -> requireLogin();
+
+	// Giriş Durumu
+	if ( $ClientArea -> isLoggedIn() ) {
+		// Giriş yapıldı
+		$kayitAdi = Capsule::table("tblclients") -> where("id", "=", $ClientArea -> getUserID()) -> pluck("firstname");
+		$ClientArea -> assign("clientname", $kayitAdi);
+	}else{
+		// Eğer giriş yapılmamış ise
+		// kayıt adına ne yazılmasını isterseniz
+		// standart olarak "Ziyaretçi" olarak geçer.
+		$ClientArea -> assign("clientname", "Ziyaretçi");
+	}
+
+	// TPL dosyasını yükle
+	// linux-hosting.tpl --> linux-hosting gibi düzenlenir
+	$ClientArea -> setTemplate("bayi-hosting");
+
+	$ClientArea -> output();
+?>
